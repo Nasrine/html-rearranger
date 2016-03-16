48eb09654dcf2330c69185ba0e2f43a318dfee8a
Nasrine <azar0006@algonquinlive.com>
2016-03-16 11:59:23 -0400

    angel brackets was missing

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-var $h1Below = $('h1');
-var $h1Above = $('h1');
+var $h1Below = $('<h1>');
+var $h1Above = $('<h1>');
+var $liBelow = $('<ul>');
+var $liAbove = $('<ul>');
 
 $h1Below.html('Below Ground Veggies');
 $('body').append($h1Below);
-
-$h1Above.html('Above Ground Veggies');
-$('body').append($h1Above);
-
-var $liBelow = $('ul');
-var $liAbove = $('ul');
-
-$h1Below.html('Below Ground Veggies');
 $('body').append($liAbove);
 
 $h1Above.html('Above Ground Veggies');
+$('body').append($h1Above);
 $('body').append($liAbove);
 
 $('ul li' ). each(function (){
   if ($(this).hasClass('below')) {
-    $liAbove.append($(this)); }
-    else{
+    $liAbove.append($(this));
+    } else {
     $liBelow.append($(this));
   }
-}
-);
+});
 
 
 
