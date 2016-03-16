ae5d90a17ba5ad1adeda65c718bb8120e6cd89da
Nasrine <azar0006@algonquinlive.com>
2016-03-16 11:21:41 -0400

    h1 and ul for below and Above done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $h1Below = $('h1');
+var $h1Above = $('h1');
+
+$h1Below.html('Below Ground Veggies');
+$('body').append($h1Below);
+
+$h1Above.html('Above Ground Veggies');
+$('body').append($h1Above);
+
+var $liBelow = $('ul');
+var $liAbove = $('ul');
+
+$h1Below.html('Below Ground Veggies');
+$('body').append($liAbove);
+
+$h1Above.html('Above Ground Veggies');
+$('body').append($liAbove);
+
+
+
